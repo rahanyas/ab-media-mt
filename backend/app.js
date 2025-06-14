63991310c6eec3a7bb5b78925207e9e37de02ea2
unknown <rahanyas3@gmail.com>
2025-06-14 18:27:45 +0530

    changig cors in bakcend

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ import travelRoutes from './routes/info.routes.js'
 const port = 9000;
 
 const app = express();
+
 app.use(cors({
-    origin : ['http://localhost:5173', 'https://ab-media-mt.vercel.app/'],
+    origin : ['http://localhost:5173', 'https://ab-media-mt.vercel.app'],
     credentials : true 
 }));
+
 app.use('/api', travelRoutes);
 
 app.listen(port, () => {
